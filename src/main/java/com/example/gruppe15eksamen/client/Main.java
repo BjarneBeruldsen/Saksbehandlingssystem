9311588ac3723409c0fa56815def89190196483c
Laurent Zogaj <143036376+26zl@users.noreply.github.com>
2025-05-19 12:23:51 +0200

    Endring

--- src/main/java/com/example/gruppe15eksamen/client/Main.java
+++ src/main/java/com/example/gruppe15eksamen/client/Main.java
@@ -2,14 +2,16 @@ package com.example.gruppe15eksamen.client;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-
+    public void start(Stage stage) {
+        Scene scene = new Scene(new Label("Hei, verden!"), 300, 200);
+        stage.setTitle("Testapp");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.gruppe15eksamen {
     requires javafx.controls;
-
+    requires javafx.fxml;
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    exports com.example.gruppe15eksamen;
+    opens com.example.gruppe15eksamen.client to javafx.fxml;
+    exports com.example.gruppe15eksamen.client;
 }
