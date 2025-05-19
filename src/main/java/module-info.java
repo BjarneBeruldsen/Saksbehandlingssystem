cc805848a397f3d45ec10160be40b2c5d400ca39
Laurent Zogaj <143036376+26zl@users.noreply.github.com>
2025-05-19 13:38:32 +0200

    Endring i modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,18 @@
 module com.example.gruppe15eksamen {
     requires javafx.controls;
-    requires javafx.fxml;
+    requires java.sql;
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.gruppe15eksamen.client to javafx.fxml;
+    //Må kalle på package i de ulike filene (undermappene)
     exports com.example.gruppe15eksamen.client;
+    //exports com.example.gruppe15eksamen.client.controller;
+   // exports com.example.gruppe15eksamen.client.network;
+    // exports com.example.gruppe15eksamen.client.view;
+
+    //exports com.example.gruppe15eksamen.model;
+
+    //exports com.example.gruppe15eksamen.server.dao;
+    //exports com.example.gruppe15eksamen.server.network;
+    //exports com.example.gruppe15eksamen.server.util;
 }
