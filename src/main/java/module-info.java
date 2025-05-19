d20db8b34abedc512d7ed5bb439a3854817c1b0c
Laurent Zogaj <143036376+26zl@users.noreply.github.com>
2025-05-19 14:25:45 +0200

    revert

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,15 +4,6 @@ module com.example.gruppe15eksamen {
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    //Må kalle på package i de ulike filene (undermappene)
     exports com.example.gruppe15eksamen.client;
-    //exports com.example.gruppe15eksamen.client.controller;
-   // exports com.example.gruppe15eksamen.client.network;
-    // exports com.example.gruppe15eksamen.client.view;
 
-    //exports com.example.gruppe15eksamen.model;
-
-    //exports com.example.gruppe15eksamen.server.dao;
-    //exports com.example.gruppe15eksamen.server.network;
-    //exports com.example.gruppe15eksamen.server.util;
 }
