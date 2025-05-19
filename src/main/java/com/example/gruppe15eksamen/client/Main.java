4507d1561f13849f372230da539691d39a041855
Laurent Zogaj <143036376+26zl@users.noreply.github.com>
2025-05-19 11:49:58 +0200

    Fjernet fxml

--- src/main/java/com/example/gruppe15eksamen/client/Main.java
+++ src/main/java/com/example/gruppe15eksamen/client/Main.java
@@ -1,7 +1,6 @@
 package com.example.gruppe15eksamen.client;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,8 @@
 module com.example.gruppe15eksamen {
     requires javafx.controls;
-    requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.gruppe15eksamen to javafx.fxml;
     exports com.example.gruppe15eksamen;
 }
